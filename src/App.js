9856a4ba9e4e5a18328d8783ca65860d92b671c3
B0225927 <>
2022-03-15 19:57:21 +0530

    login success

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import AggrTick from './tick.svg';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="successimpliedotp-container">
+    <img src={logo} alt="Logo" />
+    <img src={AggrTick} alt="success" />
+    <h3>
+      You have been {'  '}
+      <b>successfully logged in</b>
+    </h3>
+  </div>
   );
 }
 
