c624bbc81cba740e29fa63d49f50987494099ad2
B0225927 <>
2022-03-24 01:41:29 +0530

    authorization result detection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import AggrTick from './tick.svg';
 import './App.scss';
 
 function App() {
+  const STATUS={
+    ERR:'ERR',
+    SUCCESS:'SUCCESS',
+  }
+  const [status, setStatus] =useState();
+
+  useEffect(()=>{
+    if(window.location.href.includes('error')){
+      setStatus(STATUS.ERR);
+    }
+    else setStatus(STATUS.SUCCESS);
+  },[]);
+  
   return (
     <div className="successimpliedotp-container">
     <img src={logo} alt="Logo" />
-    <img src={AggrTick} alt="success" />
-    <h3>
-      You have been {'  '}
-      <b>successfully logged in</b>
-    </h3>
+    {status===STATUS.SUCCESS?(
+      <>
+      <img src={AggrTick} alt="success" />
+      <h3>
+        You have been {'  '}
+        <b>successfully logged in</b>
+      </h3>
+    </>
+    ):(
+      <>
+      <img src={AggrTick} alt="fail" />
+      <h3 className='fail'>
+        You have been {'  '}
+        <b>logged out</b>
+      </h3>
+    </>
+    )}
   </div>
   );
 }
